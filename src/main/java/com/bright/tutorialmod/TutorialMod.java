156b37b5701f77cded4d0625a3d3166570f74f99
CinammonToast <professotr56@gmail.com>
2022-10-11 12:41:27 +0300

    Testing the push feature, just added a comment

--- src/main/java/com/bright/tutorialmod/TutorialMod.java
+++ src/main/java/com/bright/tutorialmod/TutorialMod.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
+// this is a comment to test teh github
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
