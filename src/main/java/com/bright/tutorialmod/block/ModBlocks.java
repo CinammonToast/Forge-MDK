d027de30098360efdfb2b7c4fe76243d031bf99c
CinammonToast <professotr56@gmail.com>
2022-10-12 14:23:25 +0300

    TRYING TO FIND AN ERROR

--- src/main/java/com/bright/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/bright/tutorialmod/block/ModBlocks.java
@@ -44,6 +44,7 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(4f)), ModCreativeModeTab.TUTORIAL_TAB);
 
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T>block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
